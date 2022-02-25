0d2c0b1ee769574c68d494e7b9b0a119d53dbec9
赵蓬杨 <378278978@qq.com>
2022-02-26 00:31:42 +0800

    mobile

--- src/settings.js
+++ src/settings.js
@@ -1,1 +1,1 @@
-window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/LoadingScene.fire",orientation:"portrait",jsList:[]};
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/LoadingScene.fire",orientation:"portrait",jsList:[]};
